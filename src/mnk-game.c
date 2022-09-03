d6dd85e6fc7845914ccceb1fc533c874985901fa
tepn <pioupioutte@gmail.com>
2022-09-03 23:22:12 +0200

    v0.2.0: - Add game mechanic - Add display - Clean code

--- src/mnk-game.c
+++ src/mnk-game.c
@@ -4,15 +4,401 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include <assert.h>
+#include <math.h>
 #include <getopt.h>
+#include <string.h>
 
 /* Set default values */
 static bool verbose = false;
-static int m = 3;
-static int n = 3;
-static int k = 3;
+static int m = 3;    /* Number of rows */
+static int n = 3;    /* Number of columns */
+static int k = 3;    /* Number of stones aligned to win */
 
-static void usage(int status) {
+/* Functions */
+static int
+str2index(char str[BUFFER_SIZE])
+{
+	/* Transform a string to decimal.
+	   E.g:
+	   decimal2index("002") = 1
+	   decimal2index("101") = 100  */
+	return atoi(str) - 1;
+}
+
+static char*
+index2str(char buffer[BUFFER_SIZE], int size, int index)
+{
+	/* Transform a decimal to string of a given size.
+
+	   E.g:
+	   index2str(buffer, 3, 5) = "006"
+	   index2str(buffer, 2, 10) = "11"  */
+	int n = floor(log10(index)) + 1;    // Number of digit in index
+	int f = 0;                          // Use for flag
+
+	/* assert */
+	assert(n <= size);
+	assert(n <= BUFFER_SIZE);
+
+	f = snprintf(buffer, size + 1, "%.*d", size, index + 1);
+
+	if (f < 0)
+		return NULL;
+
+	return buffer;
+}
+
+static char**
+board_alloc()
+{
+	/* Allocation of a board of size m by n */
+	char** board = calloc(m, sizeof(char*));
+
+	if (board == NULL)
+	{
+		/* Error in allocation */
+		perror("Error allocation");
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		board[i] = calloc(n, sizeof(char));
+		if (board[i] == NULL)
+		{
+			/* Error in allocation */
+			perror("Error allocation");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			board[i][j] = NONE;
+		}
+	}
+
+	return board;
+}
+
+static void
+board_free(char** board)
+{
+	/* Free the board */
+	for (int i = 0; i < m; i++)
+	{
+		free(board[i]);
+	}
+
+	free(board);
+}
+
+static void
+board_print(char** board)
+{
+	/* Print the state of the board */
+	int digits_m = floor(log10(m)) + 1;    // Number of digit in m
+	int digits_n = floor(log10(n)) + 1;    // Number of digit in n
+	char buffer[BUFFER_SIZE];
+
+	/* Header */
+	for (int i = 0; i < digits_m; i++)
+	{
+		printf("%s", " ");
+	}
+
+	for (int j = 0; j < n; j++)
+	{
+		index2str(buffer, digits_n, j);
+		printf(" %s", buffer);
+	}
+
+	printf("\n");
+
+	/* Content */
+	for (int i = 0; i < m; i++)
+	{
+		index2str(buffer, digits_m, i);
+		printf("%s", buffer);
+
+		for (int j = 0; j < n; j++)
+		{
+			for (int ind = 0; ind < digits_n; ind++)
+			{
+				printf("%s", " ");
+			}
+
+			printf("%c", board[i][j]);
+
+		}
+
+		printf("\n");
+
+	}
+
+	printf("\n");
+
+}
+
+static int
+board_set(char stone, int i, int j, char** board)
+{
+	/* set a stone on board [i][j]
+	   return 0 if valid, -1 otherwise. */
+	if ((i < 0) || (j < 0) || (i >= m) ||(j >= n) || board[i][j] != NONE)
+	{
+		return -1;
+	}
+	else
+	{
+		/* Set the stone */
+		board[i][j] = stone;
+		// turn++
+		return 0;
+	}
+}
+
+static char
+is_winner(char** board)
+{
+	/* Return the winner of the board, Character NONE otherwise. */
+	int count;
+	char winner = NONE;
+
+	/* Read rows */
+	for (int i = 0; i < m; i++)
+	{
+		count = 0;
+
+		for (int j = 0; j < n; j++)
+		{
+			char cursor = board[i][j];
+
+			if (cursor == PLAYER1)
+			{
+				if (winner == PLAYER1)
+				{
+					count++;
+				}
+				else
+				{
+					count = 1;
+					winner = PLAYER1;
+				}
+			}
+			else if (cursor == PLAYER2)
+			{
+
+				if (winner == PLAYER2)
+					count++;
+				else
+				{
+					count = 1;
+					winner = PLAYER2;
+				}
+
+			}
+			else
+				count = 0;
+
+			if (count == k)
+			{
+				printf("Test: Row\n");
+				return winner;
+			}
+		}
+	}
+
+	/* Read columns */
+	for (int j = 0; j < n; j++)
+	{
+		count = 0;
+
+		for (int i = 0; i < m; i++)
+		{
+			char cursor = board[i][j];
+
+			if (cursor == PLAYER1)
+			{
+				if (winner == PLAYER1)
+					count++;
+				else
+				{
+					count = 1;
+					winner = PLAYER1;
+				}
+			}
+			else if (cursor == PLAYER2)
+			{
+
+				if (winner == PLAYER2)
+					count++;
+				else
+				{
+					count = 1;
+					winner = PLAYER2;
+				}
+
+			}
+			else
+				count = 0;
+
+			if (count == k)
+				return winner;
+		}
+	}
+
+	/* Read diagonally (from upper right to bottom left) */
+	for (int i = 0; i < m - k + 1; i++)
+	{
+		count = 0;
+
+		for (int j = k - 1; j > n; j++)
+		{
+			char cursor = board[i][j];
+
+			if (cursor == NONE)
+				count = 0;
+			else
+			{
+				winner = cursor;
+				for (int l = 0; l < k; l++)
+				{
+					if (board[i+l][j+l] == winner)
+						count++;
+					else
+						count = 0;
+				}
+
+				if (count == k)
+					return winner;
+			}
+		}
+	}
+
+	/* Read diagonally (from upper left to bottom right) */
+	for (int i = 0; i < m - k + 1; i++)
+	{
+		count = 0;
+
+		for (int j = n - k + 1; j > n; j++)
+		{
+			char cursor = board[i][j];
+
+			if (cursor == NONE)
+				count = 0;
+			else
+			{
+				winner = cursor;
+				for (int l = 0; l < k; l++)
+				{
+					if (board[i+l][j+l] == winner)
+						count++;
+					else
+						count = 0;
+				}
+
+				if (count == k)
+					return winner;
+			}
+		}
+	}
+
+	return NONE;
+}
+
+static char
+game(char** board)
+{
+	/* Mechanic of the game for a turn. */
+	bool valid = false;
+
+	printf("\nNumber of stones to align to win (k) = %i)\n\n", k);
+	board_print(board);
+
+	while (valid == false)
+	{
+		char player;
+
+		/* Get player to play */
+		int stone_X = 0;
+		int stone_O = 0;
+
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (board[i][j] == PLAYER1)
+					stone_X++;
+				else if (board[i][j] == PLAYER2)
+					stone_O++;
+			}
+		}
+
+		player = (stone_X <= stone_O) ? PLAYER1 : PLAYER2;
+
+		/* Print state of board and prompt */
+		printf("Player '%c' give your move 'line column' (eg: 1 2), "
+			"press 'Q' to quit: ", player);
+
+		/* Read the input */
+		char buffer[BUFFER_SIZE];
+		char* arg;
+		int arg_i = 0;
+
+		fgets(buffer, sizeof(buffer), stdin);
+		printf("\n");
+
+		arg = strtok(buffer, " ");
+
+		/* Get arguments */
+		int x, y;
+
+		while(arg != NULL)
+		{
+			if ((str2index(arg) != -1) && (arg_i == 0))
+				x = str2index(arg);
+			else if ((str2index(arg) != -1) && (arg_i == 1))
+			{
+				y = str2index(arg);
+				valid = true;
+			}
+			else if ((*arg == 'Q') || (*arg == 'q'))
+			{
+				board_free(board);
+				exit(EXIT_SUCCESS);
+			}
+
+			arg_i++;
+			arg = strtok(NULL, " ");
+
+		}
+
+		if (valid)
+		{
+			if (board_set(player, x, y, board) == 0)
+			{
+				if (player == PLAYER1)
+					player = PLAYER2;
+				else
+					player = PLAYER1;
+			}
+			else
+				printf("Invalid move\n");
+		}
+		else
+			printf("Invalid syntaxe\n");
+	}
+
+	return is_winner(board);
+
+}
+
+static void
+usage(int status) {
 	if (status == EXIT_SUCCESS) {
 		fputs(
 			"Usage: mnk-game [OPTION] [-c FILE]\n"
@@ -35,13 +421,16 @@ static void usage(int status) {
 	exit(status);
 }
 
-static void version(void) {
+static void
+version(void) {
 	printf("mnk-game %i.%i.%i\n"
 		"This software is an mnk-game with AI players.\n",
 		PROG_VERSION, PROG_SUBVERSION, PROG_REVISION);
 }
 
-int main(int argc, char* argv[]) {
+/* Main */
+int
+main(int argc, char* argv[]) {
 	int optc = 0;
 	
 	static struct option long_opts[] = {
@@ -126,5 +515,30 @@ int main(int argc, char* argv[]) {
 				usage(EXIT_FAILURE);
 		}
 	}
+
+	/* Game loop */
+	char** board = board_alloc();
+	int turn = 0;
+
+	while(turn < m * n)
+	{
+		char winner = game(board);
+
+		if (winner != NONE)
+		{
+			board_print(board);
+			printf("The winner is '%c' !\n", winner);
+			board_free(board);
+			exit(EXIT_SUCCESS);
+		}
+
+		turn++;
+
+	}
+
+	board_print(board);
+	printf("No winner, this is a draw.\n");
+	board_free(board);
+
 	return EXIT_SUCCESS;
 }
--- src/mnk-game.h
+++ src/mnk-game.h
@@ -3,10 +3,15 @@
 
 #define PROG_NAME "mnk-game"
 
-#define BOARD_MAX 64
-
 #define PROG_VERSION    0
-#define PROG_SUBVERSION	1
-#define PROG_REVISION   1
+#define PROG_SUBVERSION	2
+#define PROG_REVISION   0
+
+#define BOARD_MAX 64
+#define NONE '_'
+#define PLAYER1 'X'
+#define PLAYER2 'O'
+
+#define BUFFER_SIZE 8
 
 #endif /* MNK_GAME_H */
